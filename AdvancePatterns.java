fe6a67a40b0f35d452f5264ceeffbfb923c55a41
SaJid96 <sajidhussain98@gmail.com>
2022-09-10 13:44:56 +0530

    Advance pattern questions

--- AdvancePatterns.java
+++ AdvancePatterns.java
@@ -4,33 +4,75 @@ public class AdvancePatterns {
     public static void main(String[] args) {
 
         int n=5;
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces=2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+//
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n-i;j++)
+//            {
+//                System.out.print(" ");
+//            }
+//            for (int j=1;j<=i;j++){
+//                System.out.print(i+ " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//
+//            for (int j=1;j<=5;j++)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            int spaces=2*(n-i);
+//            for(int j=1;j<=spaces;j++){
+//                System.out.print(" ");
+//            }
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 //lower part
-        for (int i=n;i>=1;i--){
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces=2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for (int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        for (int i=n;i>=1;i--){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            int spaces=2*(n-i);
+//            for(int j=1;j<=spaces;j++){
+//                System.out.print(" ");
+//            }
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
     }
 }
